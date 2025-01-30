ab9cf9ef45ee607ed74e36e4a7d6a716f83c29d0
Panagiotis A. <pargyropoulos@gmail.com>
2025-01-30 02:06:11 +0200

    created a simple dashboard view

--- UniApp/src/main.java
+++ UniApp/src/main.java
@@ -1,3 +1,6 @@
+
+import view.dashboard;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -16,6 +19,8 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        dashboard mainForm = new dashboard();
+        mainForm.setVisible(true);
     }
     
 }
