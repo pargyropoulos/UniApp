db677d326ac350dbece898aeb21187acbfd5bc85
Panagiotis A. <pargyropoulos@gmail.com>
2025-02-11 02:27:43 +0200

    Search view autocloses and returns nummber of found entries in the GridView

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,9 @@ import controller.MainController;
  *
  * @author Panagiotis Argyropoulos - pargyropoulos@gmail.com or std154845@ac.eap.gr
  */
-public class Main {
-    
-    public static void main(String[] args) {
-        
+public class Main { 
+    public static void main(String[] args) {        
         MainController ctrl=new MainController(new MainView());
         ctrl.run();
     }
-    
 }
